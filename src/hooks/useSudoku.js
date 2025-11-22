76b8a419436991543f53d96f3e206948b3a5626e
Syujaaa <syujafarras@gmail.com>
2025-11-22 20:11:20 +0700

    Update medium

--- src/hooks/useSudoku.js
+++ src/hooks/useSudoku.js
@@ -5,7 +5,7 @@ import { useState, useEffect } from "react";
 // Density menentukan *chance dihapus*, tetap dipakai
 const LEVEL_MAP = {
   easy: 0.0,
-  medium: 0.35,
+  medium: 0.15,
   hard: 0.65,
   expert: 0.75,
 };
@@ -13,7 +13,7 @@ const LEVEL_MAP = {
 // Jumlah angka terbuka (clues) PASTI
 const OPEN_COUNT = {
   easy: 40,
-  medium: 18,
+  medium: 20,
   hard: 6,
   expert: 3,
 };
