bdc7bf0e2c3d432187b474a014fbcb866c9996ce
Syujaaa <syujafarras@gmail.com>
2025-12-09 21:26:12 +0700

    Update api url

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 // const BASE_URL = "http://localhost:4000";
-// const BASE_URL = "https://sudoku-api-qz9q5md99d21.syujafarras010.deno.net";
-const BASE_URL = "https://sudoku-api-ten.vercel.app";
+const BASE_URL = "https://sudoku-api-qz9q5md99d21.syujafarras010.deno.net";
+// const BASE_URL = "https://sudoku-api-ten.vercel.app";
 
 export default BASE_URL;
